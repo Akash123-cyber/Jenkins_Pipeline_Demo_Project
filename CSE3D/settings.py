8e1d4b6d83dd73600a1ec45201a566253683c963
Akash Kumar Malik <getakashmalik@gmail.com>
2026-05-09 18:50:20 +0530

    update settings.py

--- CSE3D/settings.py
+++ CSE3D/settings.py
@@ -116,11 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT ='/Jenkins_Pipeline_Demo_Project/static-files'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static-files'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/Jenkins_Pipeline_Demo_Project/media'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Default primary key field type
